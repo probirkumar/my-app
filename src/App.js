b18bb9a19f8f9a4154b08b1f020bdb38f40888cc
ProbirKumare <probirsharma655@gmail.comm>
2024-03-30 10:31:57 +0600

    using component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
 import logo from './logo.svg';
 import './App.css';
 
+const singers = [
+  {name: 'Dr. Mahafuz', job: 'Doctor'},
+  {name: 'Eva Rahman', job: 'singer'},
+  {name: 'Agun', job: 'Sopno'},
+  {name: 'Shuvro', job: 'Pathor'}
+];
+
 function App() {
+  const nayoks = ['Rubel', 'Bapparaz', 'Kuber', 'Jashim', 'Salman Shah'];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        nayoks.map(nayok => <li>Name: {nayok}</li>)
+      }
+      {/* {
+        nayoks.map(nayok => <Person name={nayok}></Person>)
+      } */}
+      {
+        singers.map(singer => <Person name={singer.name}></Person>)
+      }
+      {/* <Person name={nayoks[0]} nayika="Mousumi"></Person>
+      <Person name={nayoks[1]} nayika="cheka"></Person>
+      <Person name={nayoks[2]} nayika="Kopila"></Person> */}
+      <h4>New Component.</h4>
+      <Friend name="Ajay Devgun" phone="017777"></Friend>
+      <Friend name="Shakib Khan" phone="017777"></Friend>
     </div>
   );
 }
+
+function Person(props) {
+  // console.log(props);
+  return (
+    <div className="person">
+      <h1>{props.name}</h1>
+      <p>{props.nayika}</p>
+    </div>
+  )
+}
+
+function Friend(props) {
+  // console.log(props);
+  return (
+    <div className="container">
+      <h1>Name: {props.name}</h1>
+      <p>Phone: {props.phone}</p>
+    </div>
+  )
+}
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
